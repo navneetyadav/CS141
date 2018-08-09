5ae99afa986917274b90189b859028b1352fb302
navneetyadav <42238470+navneetyadav@users.noreply.github.com>
2018-08-09 10:51:38 +0000

    Add files via upload

---
+++ Lab1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){ 
+          cout  << "HELLO.";
+        cout  <<     " I am studying c++ in NISER.";
+        cout  << "I learnt JAVA in high school.";
+          cout  <<   "I have forgotten all the JAVA commands.";
+return 1;
+
+
+
+}
